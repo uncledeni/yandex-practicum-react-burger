d5b3c237e176afddb83d947caff75cebfcfa4751
BarDen <uncledeni@mail.ru>
2024-07-17 01:46:32 +0300

    Minor visual updates and try/catch fix
    Update 'ProfilePage' layout;
    Update 'AppHeader' navbar visuals;
    Update try/catch in 'burger-ingredients' and 'order-details' redux actions;

--- src/pages/account/components/profile-navbar/profile-navbar.jsx
+++ src/pages/account/components/profile-navbar/profile-navbar.jsx
@@ -2,10 +2,14 @@ import ProfileNavbarStyles from "./css/style.module.css";
 
 export const ProfileNavbar = () => {
     return (
-        <div className={ProfileNavbarStyles.navbarContainer}>
+        <div className={`${ProfileNavbarStyles.profileNavbar} ml-15`}>
             <p className="pt-4 pb-4 text text_type_main-medium">Профиль</p>
             <p className="pt-4 pb-4 text text_type_main-medium text_color_inactive">История заказов</p>
             <p className="pt-4 pb-4 text text_type_main-medium text_color_inactive">Выход</p>
+            <p className="pt-20 text text_type_main-default text_color_inactive">
+                В этом разделе вы можете
+                изменить свои персональные данные
+            </p>
         </div>
     )
 }
--- src/widgets/app-header/app-header.jsx
+++ src/widgets/app-header/app-header.jsx
@@ -2,31 +2,36 @@ import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
 import { BurgerIcon, ListIcon, ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 
 import AppHeaderStyles from "./css/style.module.css"
+import { NavLink } from "react-router-dom";
 
 const NAVIGATION_VALUES = [
     {
-        icon_primary: <BurgerIcon type="primary"/>,
-        icon_secondary: <BurgerIcon type="secondary"/>,
+        icon_primary: <BurgerIcon type="primary" />,
+        icon_secondary: <BurgerIcon type="secondary" />,
         text: "Конструктор"
     },
     {
-        icon_primary: <ListIcon type="primary"/>,
-        icon_secondary: <ListIcon type="secondary"/>,
+        icon_primary: <ListIcon type="primary" />,
+        icon_secondary: <ListIcon type="secondary" />,
         text: "Лента заказов"
     },
     {
-        icon_primary: <ProfileIcon type="primary"/>,
-        icon_secondary: <ProfileIcon  type="secondary"/>,
+        icon_primary: <ProfileIcon type="primary" />,
+        icon_secondary: <ProfileIcon type="secondary" />,
         text: "Личный кабинет"
     },
 ]
 
 const HeaderElem = (props) => {
     return (
-        <div className={`${AppHeaderStyles.navElem} pr-5 text text_type_main-default ${(props.inactive) && "text_color_inactive"}`}>
-            {(props.inactive) ? props.values.icon_secondary : props.values.icon_primary}
-            <p className="pl-2">{props.values.text}</p>
-        </div >
+        <NavLink to={props.link} className={({isActive}) => {
+            return `pr-5 text text_type_main-default ${(!isActive) ? "text_color_inactive" : ""}`
+        }}>
+            <div className={`${AppHeaderStyles.navElem} `}>
+                {(props.inactive) ? props.values.icon_secondary : props.values.icon_primary}
+                <p className="pl-2">{props.values.text}</p>
+            </div >
+        </NavLink>
     )
 }
 
@@ -35,13 +40,13 @@ export const AppHeader = (params) => {
         <header className={AppHeaderStyles.appHeaderWrapper}>
             <nav className={AppHeaderStyles.appHeaderContent}>
                 <div className={AppHeaderStyles.navigation}>
-                    <HeaderElem values={NAVIGATION_VALUES[0]} />
-                    <HeaderElem inactive values={NAVIGATION_VALUES[1]} />
+                    <HeaderElem link={'/'} values={NAVIGATION_VALUES[0]} />
+                    <HeaderElem link={'/b'} inactive values={NAVIGATION_VALUES[1]} />
                 </div>
                 <span className={AppHeaderStyles.logo}>
-                    <Logo/>
+                    <Logo />
                 </span>
-                <HeaderElem inactive values={NAVIGATION_VALUES[2]} />
+                <HeaderElem link={'/profile'} inactive values={NAVIGATION_VALUES[2]} />
             </nav>
         </header>
     )
